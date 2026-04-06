92dc15357270c967195558bfa7e9fe2725b4105e
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-04-06 13:34:51 +0300

    Write funcs to handle forms

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,33 @@
+import prisma from "../lib/prisma.js";
+import bcrypt from "bcrypt";
+
+// RENDER FORMS
 // Render Log In Form (GET)
 async function renderLogInForm(req, res) {
-  try {
-    res.render("logInForm");
-  } catch (error) {
-    console.error();
-    res.status(500).send("Error rendering form");
-  }
+  res.render("logInForm");
 }
-
 // Render Sign Up Form (GET)
 async function renderSignUpForm(req, res) {
+  res.render("signUpForm");
+}
+
+// FORM SUBMISSION
+// Handle Sign Up Form Submission (POST)
+async function handleSignUpForm(req, res) {
   try {
-    res.render("signUpForm");
+    // Destructure input data
+    const { email, password, confirmPassword } = req.body;
+    if (password !== confirmPassword) {
+      res.send("Passwords must match");
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await prisma.user.create({
+      data: { email: email, password: hashedPassword },
+    });
   } catch (error) {
-    console.error();
-    res.status(500).send("Error rendering form");
+    console.error(error);
+    res.status(500).status("Error registering user");
   }
 }
 
-export default { renderLogInForm, renderSignUpForm };
+export default { renderLogInForm, renderSignUpForm, handleSignUpForm };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import passport from "passport";
 import authController from "../controllers/authController.js";
 
 const authRouter = Router();
@@ -6,5 +7,16 @@ const authRouter = Router();
 // Render Forms
 authRouter.get("/login", authController.renderLogInForm);
 authRouter.get("/signup", authController.renderSignUpForm);
+
+// Handle Forms
+// Authenticate Log In
+authRouter.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/home",
+    failureRedirect: "/login",
+  }),
+);
+authRouter.post("/signup", authController.handleSignUpForm);
 
 export default authRouter;

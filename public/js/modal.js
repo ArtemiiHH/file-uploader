39579b3910e017f901c0b2b1dd3297888c43cbd5
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-04-08 18:43:03 +0300

    Remove DOCTYPE + html from each component

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,13 +1,17 @@
 const fileModal = document.getElementById("fileModal");
 const folderModal = document.getElementById("folderModal");
 
-const newFileBtn = document.getElementById("newFileBtn");
-const newFolderBtn = document.getElementById("newFolderBtn");
-
-newFileBtn.addEventListener("click", () => {
-  fileModal.classList.add("modal-container-active");
+document.getElementById("newFileBtn").addEventListener("click", () => {
+  fileModal.classList.add("active");
 });
 
-newFolderBtn.addEventListener("click", () => {
-  folderModal.classList.add("modal-container-active");
+document.getElementById("newFolderBtn").addEventListener("click", () => {
+  folderModal.classList.add("active");
+});
+
+document.querySelectorAll(".close-btn").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    fileModal.classList.remove("active");
+    folderModal.classList.remove("active");
+  });
 });

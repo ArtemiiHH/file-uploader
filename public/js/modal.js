0b81666bca86d2c7cc1a9b18ffd3a21afb2609a7
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-04-08 18:58:54 +0300

    Make modals show on button press

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,17 +1,30 @@
-const fileModal = document.getElementById("fileModal");
-const folderModal = document.getElementById("folderModal");
-
-document.getElementById("newFileBtn").addEventListener("click", () => {
-  fileModal.classList.add("active");
-});
+const fileModal = document
+  .getElementById("fileModal")
+  .closest(".modal-container");
+const folderModal = document
+  .getElementById("folderModal")
+  .closest(".modal-container");
 
 document.getElementById("newFolderBtn").addEventListener("click", () => {
-  folderModal.classList.add("active");
+  folderModal.classList.add("modal-container-active");
+});
+
+document.getElementById("newFileBtn").addEventListener("click", () => {
+  fileModal.classList.add("modal-container-active");
 });
 
 document.querySelectorAll(".close-btn").forEach((btn) => {
   btn.addEventListener("click", () => {
-    fileModal.classList.remove("active");
-    folderModal.classList.remove("active");
+    folderModal.classList.remove("modal-container-active");
+    fileModal.classList.remove("modal-container-active");
+  });
+});
+
+// Close when clicking outside the modal
+document.querySelectorAll(".modal-container").forEach((container) => {
+  container.addEventListener("click", (e) => {
+    if (e.target === container) {
+      container.classList.remove("modal-container-active");
+    }
   });
 });
